e48d6ab83cc0e5059036099bb22276a2ebc8750b
ingeniously.stupid@gmail.com <ingeniously.stupid@gmail.com@f74d4cb5-efd4-b69d-7d64-c4e12f38a6f0>
2011-09-30 03:29:57 +0000

    - Cleaned DIR ../XenoShell -- Moved source files to new DIR -- Created Makefilerce/build files to new DIR -- Cleaned Makefile

--- trunk/XenoShell/make.bat
+++
@@ -1,7 +0,0 @@
-
-
-SET PROGNAME=XenoShell
-SET BINDIR=Bin
-
-powerpc-eabi-gcc -nostdlib -Wall -Os -pipe -Wl,-s,-x,--gc-sections -Wa,-mregnames -fno-exceptions -mhard-float -Wl,-T,first.lds -Wl,-Map,%BINDIR%/%PROGNAME%.map boot.s main.c -lgcc -o %BINDIR%/%PROGNAME%.elf
-powerpc-eabi-objcopy -O binary %BINDIR%/%PROGNAME%.elf %BINDIR%/%PROGNAME%.bin
